a9d2391590c628caf466d53c1ba6341b4ff410b4
Megan Kratz <meganbkratz@gmail.com>
2022-08-25 10:06:31 -0400

    create fetch request to send address to parser and return parsed response - works! (no error handling yet)

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,2 +1,49 @@
 /* TODO: Flesh this out to connect the form to the API and render results
    in the #address-results div. */
+
+window.onload = setSubmitBtnListener;
+
+// pick up submit button press
+
+function setSubmitBtnListener() {
+   console.log("Lookin for buttons");
+   const form = document.querySelector('form');
+   form.addEventListener('submit', event => {
+      event.preventDefault();
+      submitClicked();
+   });
+}
+
+function submitClicked() {
+   console.log("Submit button clicked!");
+   //get address text
+
+   const address = document.querySelector("#address").value; //should get address string
+   console.log("address:", address)
+
+   //send address to parser
+   fetch("/api/parse?" + new URLSearchParams({address:address}).toString())
+      .then(response => {
+         console.log("we're in the then block")
+         console.log(response);
+         response.json()
+            .then(data => {
+               console.log(data);
+               populateTable(data);
+            });
+      })
+      .catch(err => {
+         console.log("we're in the catch block");
+         console.log(err);
+
+      //Handle errors
+      })
+}
+ // send GET request to api/parse/ -- use fetch   <-----NEXT!
+ // recieve results back, populate table
+
+function populateTable(data) {
+   console.log("populateTable called!");
+   console.log(data);
+
+}
--- parserator_web/views.py
+++ parserator_web/views.py
@@ -14,11 +14,17 @@ class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
+        print("AddressParse.get called!")
+        address = request.query_params['address']
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+        address_components, address_type = self.parse(address)
+        #print("parsed:", address_components, address_type)
+        return Response({'input_string':address, 'address_components':address_components, 'address_type':address_type})
 
     def parse(self, address):
+
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
+        address_components, address_type = usaddress.tag(address)
         return address_components, address_type
